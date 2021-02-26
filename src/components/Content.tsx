d0fbe7cc9b72c5734ed8103261b58645eb47116f
AmandaStahlberg <amanda.stahlberg@medieinstitutet.se>
2021-02-26 10:28:20 +0100

    basic layout for startscreen

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import background from '../assets/background.png'
+import chewbacca from '../assets/chewbacca.jpg'
+
 
 function Content() {
     return (
-        <div></div>
+        <div style={rootStyle}>
+            <img style={bgStyle} src={background} alt=""/>
+            <img style={imgStyle} src={chewbacca} alt=""/>
+        </div>
     )
+}
+
+const rootStyle:CSSProperties = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh'
+}
+
+const imgStyle:CSSProperties = {
+    display: 'flex',
+    position: 'relative',
+    alignItems: 'center',
+    borderRadius: 50,
+    border: '.3rem solid yellow',
+    height: '20rem'
+}
+
+const bgStyle:CSSProperties = {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover'
 }
 
 export default Content;
