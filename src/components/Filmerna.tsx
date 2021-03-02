9ceb57218d10e1f93fa4c685fd8e9a5d328c8f13
jv98 <jacob@vahlen.se>
2021-03-02 14:44:05 +0100

    saker

--- src/components/Filmerna.tsx
+++ src/components/Filmerna.tsx
@@ -10,34 +10,53 @@ import film6 from '../assets/film6.jpg';
 import film7 from '../assets/film7.jpg';
 import film8 from '../assets/film8.png';
 import film9 from '../assets/film9.jpeg';
-import { absoluteObject } from './css';
+
+const openModal = () =>{
+    alert('test');
+}
 
 function filmer (){
     return(
-        <div style={{...absoluteObject, ...scren}}>
-        <img style={filmescren} src={film1} alt="" /> 
-        <img style={filmescren} src={film2} alt="" /> 
-        <img style={filmescren} src={film3} alt="" /> 
-        <img style={filmescren} src={film31} alt="" /> 
-        <img style={filmescren} src={film32} alt="" /> 
-        <img style={filmescren} src={film4} alt="" /> 
-        <img style={filmescren} src={film5} alt="" /> 
-        <img style={filmescren} src={film6} alt="" /> 
-        <img style={filmescren} src={film7} alt="" /> 
-        <img style={filmescren} src={film8} alt="" /> 
-        <img style={filmescren} src={film9} alt="" /> 
+        <div style={rootstyle}> 
+            <div style={{ ...scren}}>
+                
+            <img onClick={openModal} style={filmescren} src={film1} alt="" /> 
+            <img style={filmescren} src={film2} alt="" /> 
+            <img style={filmescren} src={film3} alt="" /> 
+            <img style={filmescren} src={film31} alt="" /> 
+            <img style={filmescren} src={film32} alt="" /> 
+            <img style={filmescren} src={film4} alt="" /> 
+            <img style={filmescren} src={film5} alt="" /> 
+            <img style={filmescren} src={film6} alt="" /> 
+            <img style={filmescren} src={film7} alt="" /> 
+            <img style={filmescren} src={film8} alt="" /> 
+            <img style={filmescren} src={film9} alt="" /> 
+            <div style={filmescren}></div>
+            </div>
         </div>
     )
 }
 const filmescren: CSSProperties = {
-    height: '10rem',
-    marginTop: '10rem',
+    width: '13rem',
+    height: '20rem',
+    objectFit: 'cover',
+    border: '2px solid white',
   
  }
 const scren: CSSProperties={
-    top: '30%',
-    left:'50rem',
+    width:'70%',
+    justifyContent:'space-between',
     display:'flex',
-    flexWrap: 'wrap',
+    marginTop: '5rem',
+    flexWrap:'wrap',
+    alignItems:'center',
+    
+}
+const rootstyle: CSSProperties={
+    width:'100%',
+    display: 'flex',
+    justifyContent:'center',
+    position: 'absolute',
+    alignItems:'center',
 }
 export default filmer;
